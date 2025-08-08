d099c93ff541c5a8046e1f852a636dc19a98005e
Sebastian-Francis-Taylor <seba7204@gmail.com>
2025-08-08 22:53:50 +0200

    advantage & disadvantage system implemented, no "front end" for it yet

--- Program.fs
+++ Program.fs
@@ -15,69 +15,75 @@ let deal_damage damage_dice =
     | "d8" -> d8
     | "d10" -> d10
     | "d12" -> d12
-    | "d20" -> d20
+    | "d20" -> d20 false false
     | "d100" -> d100
     | _ -> 0
 
-let rec attack (weapon: Weapon) (turn: int) =
-    if turn <> 0 then
-        let dice_roll = d20
-        let critical_hit = if dice_roll = 20 then true else false
-        let hitbonus = weapon.Hitbonus
-        let roll_to_hit = dice_roll + hitbonus
+let rec attack (weapon: Weapon) (turn: int) (advantage: bool) (disadvantage: bool) =
+    if turn = 0 then
+        printfn "Attack is over"
 
-        printf "roll: %d hit? [y/n]? " roll_to_hit
-        let input = System.Console.ReadLine()
-        printf "\n"
+    let dice_roll = d20 advantage disadvantage    
 
-        let hit =
-            match input with
-            | "y" -> true
-            | "n" -> false
-            | _ ->
-                printfn "Invalid input"
-                attack weapon turn
-                // Code won't reach here, dummy value
-                false
+    let critical_hit = 
+        match dice_roll with
+        | 20 -> true
+        | _ -> false
+    let hitbonus = weapon.Hitbonus
+    let roll_to_hit = dice_roll + hitbonus
 
-        let damage =
-            if hit then
-                deal_damage weapon.Damage_Dice
-            else
-                weapon.Damage_On_Miss
+    printf "roll: %d hit? [y/n]? " roll_to_hit
+    let input = System.Console.ReadLine()
+    printf "\n"
 
-        printfn "you dealt %d" damage
-
-        if critical_hit then
-            attack weapon (turn + 1)
-        else
-            attack weapon (turn - 1)
-
-let rec combat_loop turn =
-
-    // loads the weapon list on every turn (memory eater)
-    let weapons_list = load_weapons "weapons.json"
-
-    if turn > 0 then
-        printf "Please select a weapon from the list "
-        // Print weapons here
-
-        let input = System.Console.ReadLine()
-
-        (*
-        add weapon selection
-        pass weapon into attack
-        find a way to avoid one massive patt
-        *)
-
-        // Change to match with weapons_list name
-        let defalt_attacks = 1
-
+    let hit =
         match input with
-        | "whack" -> attack weapon_name defalt_attacks weapon_hitbonus
+        | "y" -> true
+        | "n" -> false
         | _ ->
-            printfn "invalid input try 'whack' or 'range'"
-            combat_loop turn
+            printfn "Invalid input"
+            attack weapon turn advantage disadvantage
+            // Code won't reach here, dummy value
+            false
+
+    let damage =
+        match hit with
+        | true -> deal_damage weapon.Damage_Dice
+        | false -> weapon.Damage_On_Miss
+
+    printfn "you dealt %d" damage
+
+    match critical_hit with
+    | true -> attack weapon (turn + 1) advantage disadvantage
+    | false -> attack weapon (turn - 1) advantage disadvantage
+
+let rec combat_loop weapons_list turn =
+
+    if turn = 0 then
+        printf "Combat ended"
+
+    printf "Please select a weapon from the list "
+    // Print weapons here
+
+    let input = System.Console.ReadLine()
+
+    (*
+    add weapon selection
+    pass weapon into attack
+    find a way to avoid one massive patt
+    *)
+
+    let weapon = get_weapons_by_name input
+
+
+    // Change to match with weapons_list name
+    let defalt_attacks = 1
+
+    match input with
+    | "whack" -> attack weapon defalt_attacks
+    | _ ->
+        printfn "invalid input try 'whack' or 'range'"
+        combat_loop weapons_list turn
 
 
 [<EntryPoint>]
